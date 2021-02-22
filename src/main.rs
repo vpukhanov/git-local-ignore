ea73a3f5dd68536983d7603c320f135ead6f93db
DanielRivasMD <danielrivasmd@gmail.com>
2021-02-22 12:41:43 +0100

    add manual colors & exit if manual

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{crate_authors, crate_version, App, AppSettings, Arg};
 use clap_generate::generators::*;
 use clap_generate::{generate, Generator};
+use colored::*;
 use std::env;
 use std::io;
 use std::process::exit;
@@ -107,7 +108,58 @@ fn main() {
     }
 
     if let Some(matches) = matches.subcommand_matches("completion") {
+        let manual = matches.is_present("manual");
+
         let shell = matches.value_of("shell").unwrap();
+
+        if manual {
+            match shell {
+              "bash" => print!(
+                "\n{sh}\n{cmd}\n\n{cmt}\n{lx}\n{lxcmd}\n\n{os}\n{oscmd}\n",
+                sh = "Bash:".blue().bold(),
+                cmd = "$ source <(chapulin AC --shell bash)".cyan(),
+                cmt = "# To load completions for each session, execute once:".yellow().dimmed(),
+                lx = "Linux:".green(),
+                lxcmd = "$ chapulin AC --shell bash > /etc/bash_completion.d/chapulin".cyan(),
+                os = "MacOS:".green(),
+                oscmd = "$ chapulin AC --shell bash > /usr/local/etc/bash_completion.d/chapulin".cyan(),
+                ),
+
+              "elvish" => print!("\n{}\n{}\n", "Documentation not available. Apologies.".green(), "Come back soon...".cyan(), ),
+
+              "fish" => print!(
+                "\n{sh}\n\n{cmd1}\n\n{cmt}\n{cmd2}\n",
+                sh = "Fish:".blue().bold(),
+                cmd1 = "$ chapulin AC --shell fish | source".cyan(),
+                cmt = "# To load completions for each session, execute once:".yellow().dimmed(),
+                cmd2 = "$ chapulin AC --shell fish > ~/.config/fish/completions/chapulin.fish".cyan(),
+              ),
+
+              "powershell" => print!(
+                "\n{sh}\n\n{cmd1}\n\n{cmt1}\n{cmd2}\n\n{cmt2}\n",
+                sh = "Powershell:".blue().bold(),
+                cmd1 = "PS> chapulin AC --shell powershell | Out-String | Invoke-Expression".cyan(),
+                cmt1 = "# To load completions for every new session, run:".yellow().dimmed(),
+                cmd2 = "PS> chapulin AC --shell powershell > chapulin.ps1".cyan(),
+                cmt2 = "# and source this file from your powershell profile".yellow().dimmed(),
+              ),
+
+              "zsh" => print!(
+                "\n{sh}\n\n{cmt1}\n{cmt2}\n{cmd1}\n\n{cmt3}\n{cmd2}\n\n{cmt4}\n",
+                sh = "Zsh:".blue().bold(),
+                cmt1 = "# If shell completion is not already enabled in your environment you will need".yellow().dimmed(),
+                cmt2 = "# to enable it.  You can execute the following once:".yellow().dimmed(),
+                cmd1 = "$ echo \"autoload -U compinit; compinit\" >> ~/.zshrc".cyan(),
+                cmt3 = "# To load completions for each session, execute once:".yellow().dimmed(),
+                cmd2 = "$ chapulin AC --shell zsh > \"${fpath[1]}/_chapulin\"".cyan(),
+                cmt4 = "# You will need to start a new shell for this setup to take effect".yellow().dimmed(),
+              ),
+
+              _ => panic!("Unknown generator"),
+            }
+
+            exit(0);
+        }
 
         let mut app = cli_gl();
         match shell {
@@ -117,55 +169,6 @@ fn main() {
             "powershell" => print_completions::<PowerShell>(&mut app),
             "zsh" => print_completions::<Zsh>(&mut app),
             _ => panic!("Unknown generator"),
-        }
-
-        if matches.is_present("manual") {
-            match shell {
-              "bash" => print!(
-                "\n{sh}\n{cmd}\n\n{cmt}\n{lx}\n{lxcmd}\n\n{os}\n{oscmd}\n",
-                sh = "Bash:",
-                cmd = "$ source <(git-local-ignore completion --shell bash)",
-                cmt = "# To load completions for each session, execute once:",
-                lx = "Linux:",
-                lxcmd = "$ git-local-ignore completion --shell bash > /etc/bash_completion.d/git-local-ignore",
-                os = "MacOS:",
-                oscmd = "$ git-local-ignore completion --shell bash > /usr/local/etc/bash_completion.d/git-local-ignore",
-                ),
-
-              "elvish" => print!("\n{}\n{}\n", "Documentation not available. Apologies.", "Come back soon...", ),
-
-              "fish" => print!(
-                "\n{sh}\n\n{cmd1}\n\n{cmt}\n{cmd2}\n",
-                sh = "Fish:",
-                cmd1 = "$ git-local-ignore completion --shell fish | source",
-                cmt = "# To load completions for each session, execute once:",
-                cmd2 = "$ git-local-ignore completion --shell fish > ~/.config/fish/completions/git-local-ignore.fish",
-              ),
-
-              "powershell" => print!(
-                "\n{sh}\n\n{cmd1}\n\n{cmt1}\n{cmd2}\n\n{cmt2}\n",
-                sh = "Powershell:",
-                cmd1 = "PS> git-local-ignore completion --shell powershell | Out-String | Invoke-Expression",
-                cmt1 = "# To load completions for every new session, run:",
-                cmd2 = "PS> git-local-ignore completion --shell powershell > git-local-ignore.ps1",
-                cmt2 = "# and source this file from your powershell profile",
-              ),
-
-              "zsh" => print!(
-                "\n{sh}\n\n{cmt1}\n{cmt2}\n{cmd1}\n\n{cmt3}\n{cmd2}\n\n{cmt4}\n",
-                sh = "Zsh:",
-                cmt1 = "# If shell completion is not already enabled in your environment you will need",
-                cmt2 = "# to enable it.  You can execute the following once:",
-                cmd1 = "$ echo \"autoload -U compinit; compinit\" >> ~/.zshrc",
-                cmt3 = "# To load completions for each session, execute once:",
-                cmd2 = "$ git-local-ignore completion --shell zsh > \"${fpath[1]}/_chapulin\"",
-                cmt4 = "# You will need to start a new shell for this setup to take effect",
-              ),
-
-              _ => panic!("Unknown generator"),
-            }
-
-            exit(0);
         }
     }
 }
